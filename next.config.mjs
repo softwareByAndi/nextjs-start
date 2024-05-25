6c22e4e54b0a49acba5d5a28bb4c49dcaeaabc14
softwarebyandi <softwarebyandi@gmail.com>
2024-05-24 19:22:18 -0600

    update config

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: "standalone",
+    devIndicators: {
+        autoPrerender: false
+    },
+    experimental: {
+        ipv6Enabled: true
+    }
 };
 
 export default nextConfig;
